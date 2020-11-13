3f7cfd95bd874d2dc1c341c3101c6d1a9f8b0481
Shams Ahmed <shamsahmed@me.com>
2020-11-13 10:23:13 +0000

    initial commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Shams Ahmed on 13/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YouSee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouSee
+//
+//  Created by Shams Ahmed on 13/11/2020.
+//  Copyright © 2020 io.bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
